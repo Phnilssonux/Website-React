b048f39d4a1acb3448128d22766bc181db0b71ee
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-11-19 17:22:48 +0100

    first commit

---
+++ my-react-app/src/components/profile.jsx
@@ -0,0 +1,14 @@
+import profilePicture from '../assets/jaglångt.jpeg'
+
+function Profile () {
+return (
+    <>
+    <img src={profilePicture} alt="profile picture" />
+    <h2>Philip</h2>
+    <p>Random text yala</p>
+    </>
+)
+
+}
+
+export default Profile;
