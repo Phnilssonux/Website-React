1c79035bc251733e800177ae54bece696fcf5adb
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-11-22 01:11:49 +0100

    added navbar

--- my-react-app/src/app.jsx
+++ my-react-app/src/app.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/home.jsx';
 import Collection from './Pages/collection.jsx';
+import About from './Pages/about.jsx';
+import Navbar from './navbar.jsx';
 
 function App() {
   return (
     <>
        <BrowserRouter>
+       <Navbar></Navbar>
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/collection" element={<Collection/>}/>
+            <Route path="/about" element={<About/>}/>
          </Routes>
        </BrowserRouter>  
     </>
