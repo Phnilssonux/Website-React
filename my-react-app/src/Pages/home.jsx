d93ee6a91827458c59e908595e67beb6b2c30a5f
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-11-22 01:30:16 +0100

    added button with click function

--- my-react-app/src/Pages/home.jsx
+++ my-react-app/src/Pages/home.jsx
@@ -1,5 +1,21 @@
 function Home() {
-    return <div>Homepage</div>
+    let count = 0;
+    const handleEvent = (name) => {
+        if (count < 3 ) {
+          count++;
+          console.log(`${name} clicked me ${count} time/s`);
+        } else {
+          console.log("stop clicking me!")
+        } 
+    }
+
+    return (
+    <div>
+        <button className="home-btn" onClick={() => handleEvent("User")}>
+            Click me!
+        </button>
+    </div>
+  );  
 }
 
 export default Home;
